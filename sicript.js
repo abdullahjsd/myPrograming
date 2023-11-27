9c5644c461736ffd1e953b2a2aae144642ab75dc
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-11-28 02:17:16 +0300

    js

--- sicript.js
+++ sicript.js
@@ -1,29 +1,67 @@
-const apiEndpoint = "https://api.diyanet.gov.tr/v1/prayerTimes";
-const apiKey = "YOUR_API_KEY"; // Bu kısmı kendi API anahtarınızla güncelleyin
-const cityCode = "ANKARA"; // İstenilen şehirin Diyanet İşleri Başkanlığı şehir kodu
-
-// Namaz vakitlerini çekmek için API'yi çağıran fonksiyon
-async function getPrayerTimes() {
-    try {
-        const response = await fetch(`${apiEndpoint}?city=${cityCode}`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Apikey": apiKey
-            }
+document.addEventListener("DOMContentLoaded", function () {
+    let iconElement = document.getElementById("icon");
+  
+    iconElement.addEventListener("click", function () {
+      iconElement.style.display = "none";
+      
+      let bar = document.querySelector(".bar");
+      let xmark = document.querySelector(".fa-xmark");
+  
+      if (bar) {
+        bar.style.display = "block";
+  
+        xmark.addEventListener("click", function () {
+            bar.style.display = "none";
+          iconElement.style.display = "block";
         });
+      }
+    });
+  });
+  
 
-        if (!response.ok) {
-            throw new Error("API isteği başarısız.");
-        }
 
-        const data = await response.json();
-        console.log("Namaz Vakitleri:", data);
+// document.addEventListener("click", function(){
+//     document.querySelector(".fa-bars").style.display="none";
+    
+//   });
 
-        // Burada data nesnesini kullanarak istediğiniz işlemleri yapabilirsiniz.
-    } catch (error) {
-        console.error("Hata:", error.message);
-    }
-}
+//    fetch("https://ezanvakti.herokuapp.com/vakitler?ilce=9695")
+//    .then(response => response.json())
+//    .then(responseJson => {
 
-// Namaz vakitlerini çekme fonksiyonunu çağır
-getPrayerTimes();
+//     console.log(responseJson[0])
+
+ 
+//    })
+//    .catch(error => {
+//      console.error("Hata oluştu:", error);
+//    });
+
+
+//!     TARİH  SAAT
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const  bugun = new Date();
+
+    const gun = bugun.getDate();
+    const ay = bugun.getMonth() + 1;
+    const yil = bugun.getFullYear();
+    
+    const tarihGosterElementi = document.getElementById("date");
+    tarihGosterElementi.textContent ="Tarih: " + gun +  "/" + ay + "/" + yil;
+
+    saatGoster();
+  
+    setInterval(saatGoster, 1000);
+  });
+  
+function saatGoster() {
+    let simdikiSaat = new Date();
+    
+    let saat = simdikiSaat.getHours();
+    let dakika = simdikiSaat.getMinutes();
+    let saniye = simdikiSaat.getSeconds();
+    
+    let saatGosterElementi = document.getElementById("time");
+    saatGosterElementi.textContent = saat + ":" + dakika + ":" + saniye;
+  }
